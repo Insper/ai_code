e823c4aae49521c3c85f16dd89d366b9e112724b
guidiamond <guilherme.batista770@gmail.com>
2022-09-22 11:23:10 -0300

    Add it tests para o map

--- src/test_Map.py
+++ src/test_Map.py
@@ -1,8 +1,8 @@
 from Map import *
+from aicode.search.SearchAlgorithms import AEstrela
 
 Map.createArea()
 Map.createHeuristics()
-
 
 def test_is_goal_retorna_corretamente_0():
     print('\n#### Test metodo is_goal retorna corretamente 0 ####')
@@ -23,3 +23,24 @@ def test_successors_retorna_corretamente():
     ]
     for i in range(len(expected_values)):
         assert expected_values[i] == state[i].__dict__
+
+def test_algoritmo_encontra_solucao_existente():
+    print('\n#### Teste algoritmo encontra solucao que existe ####')
+    state = Map('i', 0, 'i', 'x')
+    algorithm = AEstrela()
+    res = algorithm.search(state)
+    assert res != None
+
+def test_algoritmo_encontra_melhor_solucao_possivel_a_o():
+    print('\n#### Teste algoritmo encontra solucao de a para o ####')
+    state = Map('a', 0, 'a', 'o')
+    algorithm = AEstrela()
+    res = algorithm.search(state)
+    assert res.g == 8
+
+def test_algoritmo_encontra_melhor_solucao_possivel_n_x():
+    print('\n#### Teste algoritmo encontra solucao de n para x ####')
+    state = Map('n', 0, 'n', 'x')
+    algorithm = AEstrela()
+    res = algorithm.search(state)
+    assert res.g == 3
