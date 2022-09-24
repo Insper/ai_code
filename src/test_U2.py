3ad16f1c1bfcb481afa6a60badf312b4ecbe0c2b
Lahiry <raphaellcmb@al.insper.edu.br>
2022-09-24 13:18:12 -0300

    Adding U2 test cases

--- src/test_U2.py
+++ src/test_U2.py
@@ -7,3 +7,23 @@ def test_menor_caminho():
     result = algorithm.search(state)
     assert result.g <= 17
     print(result.show_path())
+
+def test_sem_solucao_01():
+    state = U2(True, True, True, True, False, ' ')
+    algorithm = BuscaCustoUniforme()
+    result = algorithm.search(state)
+    assert result == None
+
+def test_sem_solucao_02():
+    state = U2(False, False, False, False, True, ' ')
+    algorithm = BuscaCustoUniforme()
+    result = algorithm.search(state)
+    assert result == None
+
+def test_custo_excedente():
+    state = U2(False, False, False, True, True, ' ')
+    algorithm = BuscaCustoUniforme()
+    result = algorithm.search(state)
+    assert result.g > 17
+    print(result.show_path())
+
