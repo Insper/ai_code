d376326c1b48acddc9789ff9db8eb7b31a6a3bff
Paulo Wook Kim <61331156+Paulokim1@users.noreply.github.com>
2022-11-22 15:16:56 -0300

    subindo atv de testes automatizados (#31)
    Co-authored-by: Paulo Kim <paulokim@Paulos-MacBook-Pro.local>

--- src/test_8Puzzle.py
+++ src/test_8Puzzle.py
@@ -1,17 +1,28 @@
 from Puzzle8 import Puzzle8
 from datetime import datetime
 
+tabuleiro_feito = [[0,1,2],[3,4,5],[6,7,8]]
 tabuleiro_trivial = [[1,2,3],[8,4,0],[7,6,5]]
 tabuleiro_facil = [[8,1,3],[0,7,2],[6,5,4]]
 tabuleiro_dificil0 = [[8,3,6],[7,5,4],[2,1,0]]
 tabuleiro_dificil1 = [[7,8,6],[2,3,5],[1,4,0]]
 tabuleiro_dificil2 = [[7,8,6],[2,3,5],[0,1,4]]
 tabuleiro_dificil3 = [[3,1,2],[5,4,8],[0,6,7]]
+tabuleiro_dificil4 = [[2,8,6],[7,3,5],[4,0,1]]
 tabuleiro_impossivel1 = [[3,4,8],[1,2,5],[7,0,6]]
 tabuleiro_impossivel2 = [[5,4,0],[6,1,8],[7,3,2]]
 tabuleiro_impossivel3 = [[1,7,2],[3,9,5],[6,4,8]]
 tabuleiro_impossivel4 = [[3,1,2],[5,4,8],[0,7,6]]
 tabuleiro_invertido = [[8,7,6],[5,0,4],[3,2,1]]
+
+def test_feito():
+    print('feito')    
+    inicio = datetime.now()
+    state = Puzzle8(tabuleiro_feito, '')
+    r = state.show_path()
+    fim = datetime.now()
+    print(fim - inicio)
+    assert r == "Nao tem solucao"
 
 def test_trivial():
     print('trivial')    
@@ -40,6 +51,15 @@ def test_facil2():
     print(fim - inicio)
     assert r == " ; direita ; direita ; baixo ; esquerda ; esquerda ; cima ; cima ; direita ; baixo"
 
+def test_facil3():
+    print('facil')    
+    inicio = datetime.now()
+    state = Puzzle8(tabuleiro_facil, '')
+    r = state.show_path()
+    fim = datetime.now()
+    print(fim - inicio)
+    assert r == " ; direita ; direita ; baixo ; esquerda ; esquerda ; cima ; cima ; direita ; baixo"
+
 def test_dificil0():
     print('dificil 0')    
     inicio = datetime.now()
@@ -47,7 +67,7 @@ def test_dificil0():
     r = state.show_path()
     fim = datetime.now()
     print(fim - inicio)
-    assert r == " ; cima ; esquerda ; esquerda ; baixo ; direita ; cima ; direita ; cima ; esquerda ; esquerda ; baixo ; baixo ; direita ; cima ; direita ; baixo ; esquerda ; cima ; cima ; esquerda ; baixo ; direita"
+    assert r == " ; cima ; cima ; esquerda ; baixo ; esquerda ; baixo ; direita ; cima ; cima ; esquerda ; baixo ; baixo ; direita ; cima ; direita ; baixo ; esquerda ; cima ; cima ; esquerda ; baixo ; direita"
                 
 def test_dificil1():
     print('dificil 1')
@@ -65,7 +85,7 @@ def test_dificil2():
     r = state.show_path()
     fim = datetime.now()
     print(fim - inicio)
-    assert r == " ; cima ; cima ; direita ; baixo ; esquerda ; baixo ; direita ; cima ; direita ; cima ; esquerda ; esquerda ; baixo ; baixo ; direita ; cima ; direita ; baixo ; esquerda ; cima ; cima ; esquerda ; baixo ; direita"
+    assert r == " ; cima ; cima ; direita ; baixo ; esquerda ; baixo ; direita ; cima ; direita ; cima ; esquerda ; esquerda ; baixo ; baixo ; direita ; cima ; cima ; esquerda ; baixo ; direita ; direita ; baixo ; esquerda ; cima"
 
 def test_dificil3():
     print('dificil 3')
@@ -75,6 +95,16 @@ def test_dificil3():
     fim = datetime.now()
     print(fim - inicio)
     assert r == " ; direita ; direita ; cima ; esquerda ; esquerda ; baixo ; direita ; cima ; esquerda ; cima ; direita ; direita ; baixo ; baixo ; esquerda ; cima ; esquerda ; cima ; direita ; baixo ; direita ; cima ; esquerda ; esquerda ; baixo ; direita"
+
+def test_dificil4():
+    print('dificil 4')
+    inicio = datetime.now()
+    state = Puzzle8(tabuleiro_dificil4, '')
+    r = state.show_path()
+    fim = datetime.now()
+    print(fim - inicio)
+    assert r == " ; esquerda ; cima ; cima ; direita ; baixo ; direita ; baixo ; esquerda ; cima ; direita ; cima ; esquerda ; baixo ; esquerda ; cima ; direita ; baixo ; direita ; baixo ; esquerda ; cima"
+
 
 def test_impossivel1():
     print('impossivel 1')
